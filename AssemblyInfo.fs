613252d24325a5ee52fdfba95fe7e8c00adce471
jpeinelt <julius.peinelt@gmail.org>
2016-06-23 23:42:00 +0200

    Projektdateien hinzufügen.

---
+++ AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace GithubMock.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// Allgemeine Informationen über eine Assembly werden über die folgende 
+// Attributgruppe gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[<assembly: AssemblyTitle("GithubMock")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("GithubMock")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Durch Festlegen von ComVisible auf 'false' werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[<assembly: ComVisible(false)>]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[<assembly: Guid("df040ac3-4e21-430b-8f6b-330bd67b97e5")>]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+// 
+//       Hauptversion
+//       Nebenversion
+//       Buildnummer
+//       Revision
+// 
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [<Assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
